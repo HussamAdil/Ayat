747e52326ad5ef4425dd95406f8018614d8339a0
HussamAdil <hussam0683@gmail.com>
2022-08-15 07:08:10 +0200

    support command

--- extension.js
+++ extension.js
@@ -6,7 +6,38 @@ const axios = require('axios').default;
  * @param {vscode.ExtensionContext} context
  */ 
  async function activate(context) {
- 		
+ 		// run command GetAya 
+		 context.subscriptions.push(vscode.commands.registerCommand('ayat.getAya',async function () {
+
+			randomAyaNumber = GetrandomAyaNumber();
+	
+			let content = "";
+	
+			response = await axios.get(`http://api.alquran.cloud/v1/ayah/${randomAyaNumber}/ar.asad`).then(function (response)
+			{
+				let aya = response.data.data.text;
+	
+				let ayaNumber = response.data.data.number;
+	
+				let surah = response.data.data.surah.name
+	
+				content = `✨ ${aya} ✨ 
+			
+			💚 ${surah} (${ayaNumber})
+				` ;
+			}).catch(function (error) {
+				content = `✨  لا إله إلا أنت سبحانك إني كنت من الظالمين ✨
+				💚 لا تنسي ذكر الله
+			
+				🔴 غير متصل بالشبكة  `
+			});
+	
+			vscode.window.showInformationMessage(content);
+	
+		}));	
+
+		// autostarted 
+		
 		let repeatedEveryMinute = vscode.workspace.getConfiguration("ayat").get('repeatedEveryMinute');
 				
 		let convertMinutetoMs = repeatedEveryMinute * 60000;
