3349d07c91b7b28f6d51529accbaa5bc44e66473
HussamAdil <hussam0683@gmail.com>
2022-08-15 06:42:51 +0200

    fix repating repeating  the same aya

--- extension.js
+++ extension.js
@@ -8,37 +8,36 @@ const axios = require('axios').default;
  async function activate(context) {
  		
 		let repeatedEveryMinute = vscode.workspace.getConfiguration("ayat").get('repeatedEveryMinute');
-		
-		randomAyaNumber = GetrandomAyaNumber();
-
- 		let content = "";
-
-		response = await axios.get(`http://api.alquran.cloud/v1/ayah/${randomAyaNumber}/ar.asad`).then(function (response)
-		{
-			let aya = response.data.data.text;
-
-			let ayaNumber = response.data.data.number;
-		
-			let surah = response.data.data.surah.name
-		
-			content = `✨ ${aya} ✨ 
-		
-		💚 ${surah} (${ayaNumber})
-			` ;
-		}).catch(function (error) {
-			content = `✨  لا إله إلا أنت سبحانك إني كنت من الظالمين ✨
-
-			💚 لا تنسي ذكر الله
-		
-			🔴 غير متصل بالشبكة  `
-		});
-		
+				
 		let convertMinutetoMs = repeatedEveryMinute * 60000;
  
-		setInterval(function(){
-			vscode.window.showInformationMessage(content ,{
-			});
-		}, convertMinutetoMs);
+		setInterval( async function(){
+
+		randomAyaNumber = GetrandomAyaNumber();
+
+		let content = "";
+
+	   response = await axios.get(`http://api.alquran.cloud/v1/ayah/${randomAyaNumber}/ar.asad`).then(function (response)
+	   {
+		   let aya = response.data.data.text;
+
+		   let ayaNumber = response.data.data.number;
+	   
+		   let surah = response.data.data.surah.name
+	   
+		   content = `✨ ${aya} ✨ 
+	   
+	   💚 ${surah} (${ayaNumber})
+		   ` ;
+	   }).catch(function (error) {
+		   content = `✨  لا إله إلا أنت سبحانك إني كنت من الظالمين ✨
+
+		   💚 لا تنسي ذكر الله
+	   
+		   🔴 غير متصل بالشبكة  `
+	   });
+			vscode.window.showInformationMessage(content);
+		},convertMinutetoMs);
 		
 }
 
