d0aeb5761de3ac83e1c949ce39aa96d218cbb8b1
shakir abdo <shicolare1@gmail.com>
2022-08-23 18:05:14 +0300

    add close button to the notification

--- extension.js
+++ extension.js
@@ -3,9 +3,9 @@ const vscode = require('vscode');
 const axios = require('axios').default;
 
 function GetrandomAyaNumber() {
-	  
+
 	const ayatMinimumFromNumber = 1;
-	
+
 	const ayatMaximumAtNumber = 6236
 
 	return Math.floor(Math.random() * (ayatMaximumAtNumber - ayatMinimumFromNumber + 1)) + ayatMinimumFromNumber;
@@ -16,60 +16,60 @@ async function getRandomAya()
 	randomAyaNumber = GetrandomAyaNumber();
 
 	let content = "";
-			
+
 	try {
 
 		response = await axios.get(`http://api.alquran.cloud/v1/ayah/${randomAyaNumber}/ar.asad`)
-		
+
 		let aya = response.data.data.text;
 
 		let ayaNumber = response.data.data.numberInSurah;
 
-		let surahName = response.data.data.surah.name 
+		let surahName = response.data.data.surah.name
 
-		content = `✨${aya}✨ 
+		content = `✨${aya}✨
 	💚 ${surahName} (${ayaNumber})
 		` ;
 	} catch (error) {
 		content = `✨لا إله إلا أنت سبحانك إني كنت من الظالمين
-	
+
 		🔴 غير متصل بالشبكة  `
 	}
 
 	return content;
-} 
+}
 
 /**
  * @param {vscode.ExtensionContext} context
- */ 
+ */
 function activate(context) {
-		
+
 			context.subscriptions.push(vscode.commands.registerCommand('ayat.getAya',async function () {
-			 
+
 			getRandomAya().then(function(response){
-				vscode.window.showInformationMessage(response);
+				vscode.window.showInformationMessage(response, 'إغلاق');
 			}).catch(() => {
 				vscode.window.showInformationMessage('Error while activating Ayat :( ');
 			});
 
-		}));	
+		}));
 
-		// autostarted when vscode is up 
-		
+		// autostarted when vscode is up
+
 		let repeatedEveryMinute = vscode.workspace.getConfiguration("ayat").get('repeatedEveryMinute');
-				
+
 		let convertMinutetoMs = repeatedEveryMinute * 60000;
- 
+
 		setInterval( async function(){
 
 			getRandomAya().then(function(response){
-				vscode.window.showInformationMessage(response);
-			}).catch(function(error){ 
+				vscode.window.showInformationMessage(response, 'إغلاق');
+			}).catch(function(error){
 			vscode.window.showInformationMessage('Error while activating Ayat :( ');
 			});
-			 
+
 		},convertMinutetoMs);
-		
+
 }
 
  function deactivate() {}
